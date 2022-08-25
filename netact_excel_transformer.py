c087f2d7474023a613ad7b217b1e36eceaa46f1c
Michael_Shen <michael7105@gmail.com>
2022-08-25 12:19:37 +0800

    update

--- netact_excel_transformer.py
+++ netact_excel_transformer.py
@@ -19,13 +19,59 @@
 # root.filename =  filedialog.askopenfilenames(initialdir = "/",title = "Select file",filetypes = (("all files","*.*"), ("csv files","*.csv"),("excel files","*.xlsx")))
 # print (root.filename)
 
+## ==========
+
 from easygui import fileopenbox
 
 path_files = fileopenbox("Welcome", "COPR", filetypes= "*.txt", multiple=True)
 
 print(path_files)
 
-import openpyxl
+# import openpyxl
+# import csv
+import pandas as pd
 
-for file in path_files:
+df_result = pd.DataFrame(columns=['TYPE', 'NE', 'Severity', 'ALARM NUMBER', 'Alarm History', 'Alarm List', 'abormal alarm in weekend', 'NOTE'])
+
+for csv_file in path_files:
+    df = pd.read_csv(csv_file)
     
+    if("tas" in csv_file):
+        df_temp  = pd.DataFrame({
+            'TYPE': ['NTAS'],
+            'NE': [''],
+            'Severity': [''],
+            'ALARM NUMBER': [''],
+            'Alarm History': [''],
+            'Alarm List': [''],
+            'abormal alarm in weekend': [''],
+            'NOTE': ['']
+            })
+    df_result = pd.concat([df_temp, df_result], ignore_index = True, axis = 0)
+    
+    df_temp  = pd.DataFrame({
+            'TYPE': ['NTAS'],
+            'NE': [csv_file.split(".")[-1]],
+            'Severity': [''],
+            'ALARM NUMBER': [''],
+            'Alarm History': [''],
+            'Alarm List': [''],
+            'abormal alarm in weekend': [''],
+            'NOTE': ['']
+            })
+    df_result = pd.concat([df_temp, df_result], ignore_index = True, axis = 0)
+
+    for index, row in df.iterrows():
+        df_temp  = pd.DataFrame({
+            'TYPE': ['NTAS'],
+            'NE': [''],
+            'Severity': [df.iloc[index]['Severity']],
+            'ALARM NUMBER': [df.iloc[index]['Alarm Number']],
+            'Alarm History': ['v'],
+            'Alarm List': [''],
+            'abormal alarm in weekend': [df.iloc[index]['Alarm Text']],
+            'NOTE': ['']
+            })
+        df_result = pd.concat([df_temp, df_result], ignore_index = True, axis = 0)
+
+print(df_result)

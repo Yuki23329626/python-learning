908155a88c9ed817d1100aad902dbe6c237f24bd
Michael_Shen <michael7105@gmail.com>
2022-08-26 10:25:46 +0800

    update

--- netact_excel_transformer/netact_excel_transformer_v2.py
+++ netact_excel_transformer/netact_excel_transformer_v2.py
@@ -1,26 +1,3 @@
-# import win32com.client
-
-# # Open up Excel and make it visible
-# excel = win32com.client.Dispatch('Excel.Application')
-# excel.Visible = True
-
-# # Select a file and open it
-# file = r"C:\Users\mishen\Documents\python-learning\s3tas01(0815-0821).csv"
-# workbook = excel.Workbooks.Open(file)
-
-# # Wait before closing it
-# _ = input("Press enter to close Excel")
-# excel.Quit()
-
-# from tkinter import filedialog
-# from tkinter import *
-
-# root = Tk()
-# root.filename =  filedialog.askopenfilenames(initialdir = "/",title = "Select file",filetypes = (("all files","*.*"), ("csv files","*.csv"),("excel files","*.xlsx")))
-# print (root.filename)
-
-## ==========
-
 from asyncio.windows_events import NULL
 from contextlib import nullcontext
 from importlib.resources import path
@@ -28,16 +5,9 @@ from easygui import fileopenbox
 import pandas as pd
 from datetime import date
 import os
-# from plyer.utils import platform
 from plyer import notification
-# import openpyxl
-# import csv
 
-path_files = fileopenbox("Welcome", "COPR", filetypes= "*.txt", multiple=True)
-path_files.sort()
-print(path_files)
-
-
+# Matching file names and categorize them with a proper function name
 def match_stirng(csv_file):
     if("titan" in csv_file):
         return "NETNUMBER(ENUM)"
@@ -56,13 +26,20 @@ def match_stirng(csv_file):
     if("mw" in csv_file):
         return "OMGW"
     return ""
+
+# Get files path into list
+path_files = fileopenbox("Welcome", "COPR", filetypes= "*.txt", multiple=True)
+path_files.sort()
+print(path_files)
     
 
 df_result = pd.DataFrame(columns=['TYPE', 'NE', 'Severity', 'ALARM NUMBER', 'Alarm History', 'Alarm List', 'abormal alarm in weekend', 'NOTE'])
 
+
 for csv_file in path_files:
     df = pd.read_csv(csv_file)
 
+    # Get the duplicated index of the same function
     duplicated_index = df_result[df_result['TYPE'] == match_stirng(csv_file)].index.values
     
     if(duplicated_index.size == 0):
@@ -128,12 +105,11 @@ for csv_file in path_files:
             df_temp = pd.concat([df_temp, df_temp2], ignore_index = True, axis = 0)
         
         insertion_point = duplicated_index[0]+1
-        # print(df_result.iloc[:insertion_point])
-        # input()
         df_result = pd.concat([df_result.iloc[:insertion_point], df_temp, df_result.iloc[insertion_point:]]).reset_index(drop=True)
 
 # print(df_result)
 
+# Generate excel file with proper file name
 today = date.today()
 current_date = today.strftime("%Y%m%d")
 print("current_date =", current_date)
@@ -149,6 +125,29 @@ while os.path.exists(path_temp):
 
 df_result.to_excel(path_temp, index=False)
 print(path_temp)
+
+
+# # history
+# import win32com.client
+
+# # Open up Excel and make it visible
+# excel = win32com.client.Dispatch('Excel.Application')
+# excel.Visible = True
+
+# # Select a file and open it
+# file = r"C:\Users\mishen\Documents\python-learning\s3tas01(0815-0821).csv"
+# workbook = excel.Workbooks.Open(file)
+
+# # Wait before closing it
+# _ = input("Press enter to close Excel")
+# excel.Quit()
+
+# from tkinter import filedialog
+# from tkinter import *
+
+# root = Tk()
+# root.filename =  filedialog.askopenfilenames(initialdir = "/",title = "Select file",filetypes = (("all files","*.*"), ("csv files","*.csv"),("excel files","*.xlsx")))
+# print (root.filename)
 
 # notification.notify(
 #     title='Task has done.',

fc0816581ab5f44909abe9c4fe906d6afd5f28da
Michael_Shen <michael7105@gmail.com>
2022-08-31 15:20:53 +0800

    fix several bugs and and the component comparison

--- string_compare/config_compare_all_table.py
+++ string_compare/config_compare_all_table.py
@@ -1,6 +1,6 @@
-import re
 from easygui import fileopenbox
 
+# select the configuration files in GUI interface
 path_files = fileopenbox("Select Files", "File Read and Sort", filetypes= "*.csv", multiple=True)
 path_files.sort()
 print(path_files)
@@ -9,37 +9,44 @@ print(path_files)
 # table_name = input()
 
 for path_file in path_files:
-    res = {}
     table_dict={}
+    component_dict={}
     file1 = open(path_file, 'r')
     Lines = file1.readlines()
 
-    # for i in range(len(Lines)):
-    #     Lines[i] = Lines[i].split('.')[-1]
-    # Lines = sorted(Lines, key = lambda s: s.split(';')[1:])
-    # for i in range(len(Lines)):
-    #     Lines[i] = Lines[i].split(';')[1] + ":" + Lines[i].split(';')[2]
-    # Lines.sort()
-
-    temp = ''
+    temp_table = ''
+    temp_component = ''
     for i in range(len(Lines)):
         try:
             if(Lines[i].split(' ')[0] == '#\tTable'):
                 table_dict.update({Lines[i].split(' ')[-1].split('\n')[0]:{"start":i+2, "end":-1}})
-                if temp != '':
-                    table_dict[temp]['end'] = i - 1
-                    # print('table name: ', temp, '\nstart:', table_dict[temp]['start'], '\nend:', table_dict[temp]['end'])
+                if temp_table != '':
+                    table_dict[temp_table]['end'] = i - 1
+                    temp_table = ''
+                    # print('table name: ', temp_table, '\nstart:', table_dict[temp_table]['start'], '\nend:', table_dict[temp_table]['end'])
                     # input()
+                if temp_component != '':
+                    component_dict[temp_component]['end'] = i - 1
+                    temp_component = ''
+                temp_table = Lines[i].split(' ')[-1].split('\n')[0]
 
-                temp = Lines[i].split(' ')[-1].split('\n')[0]
             if(Lines[i].split('=')[0] == 'COMPONENT'):
-                if temp != '':
-                    table_dict[temp]['end'] = i - 1
-                    temp = ''
+                component_dict.update({Lines[i].split('=')[-1].split('\n')[0]:{"start":i+1, "end":-1}})
+                if temp_table != '':
+                    table_dict[temp_table]['end'] = i - 1
+                    temp_table = ''
+                if temp_component != '':
+                    component_dict[temp_component]['end'] = i - 1
+                    temp_component = ''
+                temp_component = Lines[i].split('=')[-1].split('\n')[0]
+
             if(i == len(Lines)):
-                if temp != '':
-                    table_dict[temp]['end'] = i
-                    temp = ''
+                if temp_table != '':
+                    table_dict[temp_table]['end'] = i
+                    temp_table = ''
+                if temp_component != '':
+                    component_dict[temp_component]['end'] = i
+                    temp_component = ''
         except IndexError:
             continue
         except Exception as err:
@@ -61,6 +68,8 @@ for path_file in path_files:
                     # print(Lines[i].split('].', 1)[1])
                     if(Lines[i].split('].')[0] not in res):
                         res.update({Lines[i].split('].')[0]:""})
+                    if(Lines[i].split('].', 1)[1].split('=')[0] == 'SequenceNumber'):
+                        continue
                     res[Lines[i].split('].')[0]] = res[Lines[i].split('].')[0]]+(Lines[i].split('].', 1)[1])
                     # print(res[Lines[i].split('].')[0]])
                 except IndexError:
@@ -80,5 +89,22 @@ for path_file in path_files:
                 j = j + 1
                 output_file.write(element)
                 output_file.write("\n")
+
+        for component_name in component_dict:
+            res = {}
+            list1 = []
+            for i in range(component_dict[component_name]['start'], component_dict[component_name]['end']):
+                try:
+                    list1.append(Lines[i].split(';',1)[-1])
+                except IndexError:
+                    list1.append(Lines[i])
+            list1 = sorted(list1)
+
+            output_file.write('Component Name: ' + component_name + '\n')
+            for element in list1:
+                if(element == ''):
+                    continue
+                output_file.write(element)
+            output_file.write("\n")
     
     print('finished')
